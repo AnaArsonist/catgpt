939e886aec104223333d4e5383553428137c0e6d
Ana <hello@anahoward.me>
2023-03-28 02:26:15 +0100

    updated metatags

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,7 +4,9 @@ export default function Document() {
   return (
     <Html className="">
       <Head>
-        <title className="">Joe</title>
+        <title className="">CatGPT</title>
+        <meta name="description" content="A virtual pet assistant." />
+        <link rel="icon" type="image/png" href="/images/cat-face.png"/>
         <body className="">
           <Main />
           <NextScript />
