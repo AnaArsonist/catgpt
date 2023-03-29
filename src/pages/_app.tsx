519072ef5cb244f6759558efc5dcd6f8c4ac2cb9
Ana <113212503+AnaArsonist@users.noreply.github.com>
2023-03-29 02:27:14 +0200

    added analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,12 @@
 import "tailwindcss/tailwind.css";
 import "@/styles/styles.css";
+import { Analytics } from "@vercel/analytics/react";
 import Script from "next/script";
 
 const App = ({ Component, pageProps }) => {
   return (
     <>
+      <Analytics />
       <link rel="preconnect" href="https://fonts.googleapis.com" />
       <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
       <link
