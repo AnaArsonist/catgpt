d806b280d713f4affec525b31cc6a811cf0c5aad
Ana <113212503+AnaArsonist@users.noreply.github.com>
2023-03-29 02:28:09 +0200

    added analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -8,7 +8,11 @@ const App = ({ Component, pageProps }) => {
     <>
       <Analytics />
       <link rel="preconnect" href="https://fonts.googleapis.com" />
-      <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+      <link
+        rel="preconnect"
+        href="https://fonts.gstatic.com"
+        crossOrigin="anonymous"
+      />
       <link
         href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;300;400;500;600;700;800;900;1000&display=swap"
         rel="stylesheet"
